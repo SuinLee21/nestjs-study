bd1a2b64739d20390dcefe2fb40f7baa70e3f268
SuinLee21 <suin7473@naver.com>
2024-12-26 19:38:19 +0900

    feat: add handler-level ValidationPipe to createBoard function

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateBoardDto {
+    @IsNotEmpty() // 빈 값일 경우 error
     title: string;
+
+    @IsNotEmpty()
     description: string;
 }
