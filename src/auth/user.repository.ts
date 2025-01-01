09d3a9c710550a4db89c61ed9c5629bcc4a0e837
SuinLee21 <suin7473@naver.com>
2025-01-01 20:57:27 +0900

    feat: Implement password hashing for user signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -11,9 +12,11 @@ export class UserRepository extends Repository<User> {
 
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
         const user = this.create({
             username,
-            password,
+            password: hashedPassword,
         })
 
         try {
